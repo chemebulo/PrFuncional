113bf06b7b2caeba4a16c0f3f286d2ab846ace37
Benjamin Maldonado <benjamindelpiero222@gmail.com>
2026-09-13 20:56:04 -0300

    Fix: Se arreglaron detalles de indentación.

--- tp-01/P1 - Conceptos Trabajados.hs
+++ tp-01/P1 - Conceptos Trabajados.hs
@@ -13,7 +13,7 @@
         > Dichas ecuaciones deben estar orientadas:
             - Lado izquierdo debe ser un nombre aún sin significado seguido de nombres de argumentos.
             - Lado derecho debe ser una expresión con significado (que puede usar los argumentos).
-        > Ejemplo:
+            Ejemplo:
 
             sumarDos n = n + 2
 
@@ -22,7 +22,7 @@
     -- Funciones anónimas:
 
         > Expresión atómica que denota directamente una función. 
-        > Ejemplo:
+            Ejemplo:
 
             (\ n -> n + 1)
 
@@ -31,7 +31,7 @@
     -- Funciones de alto orden:
 
         > Es una función que toma una función como argumento y/o devuelve una función como resultado.
-        > Ejemplo:
+            Ejemplo:
 
             twice doble
 
@@ -42,36 +42,37 @@
 
     > Es un mecanismo de ejecución, donde en una expresión, el lado izquierdo de la misma puede reemplazarse por el derecho.
     > Esto se repita hasta que no puede hacerse más.
+        Ejemplo:
 
-        ((twice twice) doble) 3
-    ->                                                      (def. twice, f <- twice)
-        (g doble) 3                                             (donde g x = twice (twice x))
-    ->                                                      (def. g, x <- doble)
-        (twice (twice doble)) 3
-    ->                                                      (def. twice, f <- twice doble)
-        g' 3                                                    (donde g' x = (twice doble) ((twice doble) x))
-    ->                                                      (def. g', x <- 3)
-        (twice doble) ((twice doble) 3) 
-    ->                                                      (def. twice, f <- doble)
-        g'' ((twice doble) 3)                                   (donde g'' x = doble (doble x))
-    ->                                                      (def. g'', x <- ((twice doble) 3))
-        doble (doble ((twice doble) 3))
-    ->                                                      (def. twice, f <- doble)
-        doble (doble (g''' 3))                                  (donde g''' x = doble (doble x))
-    ->                                                      (def. g''', x <- 3)
-        doble (doble (doble (doble 3)))
-    ->                                                      (def. doble, x <- 3)
-        doble (doble (doble (3 + 3)))
-    ->                                                      (def. doble, x <- (3 + 3))
-        doble (doble ((3 + 3) + (3 + 3)))
-    ->                                                      (aritmética)
-        doble (doble 12)
-    ->                                                      (def. doble, x <- 12)
-        doble (12 + 12)
-    ->                                                      (def. doble, x <- (12 + 12))
-        (12 + 12) + (12 + 12)
-    ->                                                      (aritmética)
-        48
+            ((twice twice) doble) 3
+        ->                                                      (def. twice, f <- twice)
+            (g doble) 3                                             (donde g x = twice (twice x))
+        ->                                                      (def. g, x <- doble)
+            (twice (twice doble)) 3
+        ->                                                      (def. twice, f <- twice doble)
+            g' 3                                                    (donde g' x = (twice doble) ((twice doble) x))
+        ->                                                      (def. g', x <- 3)
+            (twice doble) ((twice doble) 3) 
+        ->                                                      (def. twice, f <- doble)
+            g'' ((twice doble) 3)                                   (donde g'' x = doble (doble x))
+        ->                                                      (def. g'', x <- ((twice doble) 3))
+            doble (doble ((twice doble) 3))
+        ->                                                      (def. twice, f <- doble)
+            doble (doble (g''' 3))                                  (donde g''' x = doble (doble x))
+        ->                                                      (def. g''', x <- 3)
+            doble (doble (doble (doble 3)))
+        ->                                                      (def. doble, x <- 3)
+            doble (doble (doble (3 + 3)))
+        ->                                                      (def. doble, x <- (3 + 3))
+            doble (doble ((3 + 3) + (3 + 3)))
+        ->                                                      (aritmética)
+            doble (doble 12)
+        ->                                                      (def. doble, x <- 12)
+            doble (12 + 12)
+        ->                                                      (def. doble, x <- (12 + 12))
+            (12 + 12) + (12 + 12)
+        ->                                                      (aritmética)
+            48
 
 
 > Diferentes ordenes de Reducción:
@@ -79,20 +80,20 @@
     -- Orden Normal:
 
         > Se elige el redex más externo de todos.
-        > Ejemplo:
+            Ejemplo:
 
-            doble (doble 12)
-        ->                              (def. doble, x <- doble 12)
-            (doble 12) + (doble 12)
+                doble (doble 12)
+            ->                              (def. doble, x <- doble 12)
+                (doble 12) + (doble 12)
 
     -- Orden Aplicativo:
 
         > Se elige el redex más interno de todos.
-        > Ejemplo:
+            Ejemplo:
 
-            doble (doble 12)
-        ->                              (def. doble, x <- 12)
-            (doble 24)
+                doble (doble 12)
+            ->                              (def. doble, x <- 12)
+                (doble 24)
 
 
 > Utilidad del "let-in", "if-then-else", "case-of", "where":
@@ -101,7 +102,7 @@
 
         > Es una expresión que introduce variables o funciones locales válidas únicamente dentro del bloque in.
         > Evita repetir cálculos o expresiones complejas, además de permitir descomponer un problema en partes más chicas.
-        > Ejemplo:
+            Ejemplo:
 
             (\ph -> let (g1, g2) = ph in Cucurucho g1 g2) 
 
@@ -109,7 +110,7 @@
 
         > Es una expresión condicional, la cual permite elegir entre dos alternativas según una condición booleana.
         > Es obligatorio escribir la rama del else, ya que toda la estructura debe devolver un valor del tipo que retorna.
-        > Ejemplo:
+            Ejemplo:
 
             (\n -> if n > 0 then Chocolate else Frutilla) 
 
@@ -117,7 +118,7 @@
 
         > Es una expresión usada para realizar pattern matching de forma explícita sobre el valor de cualquier expresión.
         > Permite bifurcar el código según la forma o estructura del dato.
-        > Ejemplo:
+            Ejemplo:
 
             (\h -> case h of 
                         (Vasito g) -> g
@@ -127,11 +128,11 @@
 
         > Es una cláusula sintáctica usada para darle claridad a las funciones definiendo expresiones auxiliares.
         > Se adjunta al final de una función o guarda para usar bindings locales.
-        > Ejemplo:
+            Ejemplo:
 
             numeroAHelado :: Int -> Helado
             numeroAHelado n = if n > 0 
-                                 then heladoV
-                                 else heladoC
+                                    then heladoV
+                                    else heladoC
                 where heladoV = Vasito Chocolate
                       heladoC = Cucurucho Chocolate Sambayon
